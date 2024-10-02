bb9d40ea5171421c5aa0c768a3c74a06c3ec020e
Karulina Alencar de Freitas <karulina.freitas@opus-software.com.br>
2024-10-02 15:32:49 -0300

    Add project set up

---
+++ src/test/java/com/karu/mediatracker/MediatrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karu.mediatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediatrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
